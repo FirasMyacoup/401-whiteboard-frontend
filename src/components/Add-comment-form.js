c2cffdff5b1293cb4d30cd0ac42d494cb06d8099
FirasMyacoup <firas.yacoup1@gmail.com>
2022-09-19 23:55:31 +0300

    lab07

--- src/components/Add-comment-form.js
+++ src/components/Add-comment-form.js
@@ -2,23 +2,25 @@ import axios from "axios";
 import React from 'react';
 
 
-function AddCommentForm (props) {
-    const handleComment = async (e) => {
+function AddCommentForm ( props ) {
+    const handleSubmit = async ( e ) => {
         e.preventDefault();
         const comment = {
             'content': e.target.content.value,
         };
-        await axios.post(`https://post-401.herokuapp.com/${props.postId}`,comment).then( () => {
+        await axios.post(
+            `${process.env.REACT_APP_HEROKU_URL}/comment/${props.postId}`,comment
+        ).then( () => {
             props.getData();
         } );
     };
     return (
         <>
             <div className="add-comment-form">
-                <h2>Add Comment</h2>
-                <form onSubmit={handleComment}>
+                <h2>Comment Something :)</h2>
+                <form onSubmit={handleSubmit}>
                     <div className="form-control">
-                        <textarea placeholder="Add Comment " name="content"></textarea>
+                        <textarea placeholder="Add Comment content" name="content"></textarea>
                     </div>
                     <div className="form-control">
                         <input type="submit" />
